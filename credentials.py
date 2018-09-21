ee6050baa425783289eb4fba1abbb29e6b1f0054
Bernd Prager <bernd@central.tech>
2018-09-21 10:39:02 +0700

    empty credential file to be frozen

---
+++ credentials.py
@@ -0,0 +1,5 @@
+#Variables that contains the user credentials to access Twitter API 
+access_token = "ENTER YOUR ACCESS TOKEN"
+access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "ENTER YOUR API KEY"
+consumer_secret = "ENTER YOUR API SECRET"
